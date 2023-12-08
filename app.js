bd24ea99e49cd5cd6ce05b67f8e8971306fdc9e2
Alp Serin <alpsrn@gmail.com>
2023-12-08 15:49:26 +0300

    add swagger docs

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// swagger
+const swagger = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -22,14 +27,13 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1); // we need this because we will deploy our server
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
   })
 );
-
-app.set("trust proxy", 1); // we need this because we will deploy our server
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
@@ -37,8 +41,10 @@ app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send("<h1>jobs API</h1><a href='/api-docs'>Documentation</a>");
 });
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
